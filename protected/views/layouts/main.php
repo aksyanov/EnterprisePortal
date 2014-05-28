86141dae951e74095e69648ed7e5b5061fdb33c2
Ruslan <aksyanov.r@gmail.com>
2014-05-28 19:44:10 +0400

    create wrappers

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,20 +3,43 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/style/general.css">
+    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/style/main.css">
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.js"></script>
 </head>
 
 <body>
 
-    <?php
-        if(Yii::app()->user->isGuest){
-            echo 'Здравствуй, Гость!';
-        }else{
-            echo 'Здравствуй, Друг!';
-        }
-    ?>
+    <div class="top">
+        <div class="topLogo">
+            <div class="topLogoInner">
+                <a href="http://localhost/entportal/"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/logo.png" width="130" height="82" alt=""></a>
+            </div>
+        </div>
 
-    <?php echo $content; ?>
+        <div class="topMenu">
+            <nav>
+                <ul>
+                    <a href="#"><li>Главная</li></a>
+                    <a href="#"><li>Документы</li></a>
+                    <a href="#"><li>Новости</li></a>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="userInfo">
+            <?php
+                if(Yii::app()->user->isGuest){
+                    echo 'Здравствуй, Гость!';
+                }else{
+                    echo 'Здравствуй, Друг!';
+                }
+            ?>
+        </div>
+    </div>
+
+    <div class="mainWrapper">
+        <?php echo $content; ?>
+    </div>
 
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/main.js"></script>
 </body>
---
+++ protected/views/site/index.php
@@ -0,0 +1,2 @@
+asdasd
+<div style="background-color: #000000;height: 100px;width: 100px"></div>
